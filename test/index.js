00c6ac3b13e474d70a29d1cbc26238d1494df0bf
foxintellivan <ivan@foxintelligence.fr>
2019-04-29 18:33:59 +0200

    first commit

---
+++ test/index.js
@@ -0,0 +1,84 @@
+const assert = require('assert')
+const autograph = require('./../src/index')
+
+describe('graph making', () => {
+    it('object', () => {
+        const graph = autograph.line({
+            A: {
+                a: 1,
+                b: 2
+            },
+            B: {
+                a: 2,
+                b: 4
+            }
+        })
+        
+        assert.deepEqual(graph, {
+            "type": "line",
+            "data": {
+                "labels": [
+                    "A",
+                    "B"
+                ],
+                "datasets": [
+                    {
+                        "label": "a",
+                        "backgroundColor": "rgba(244, 67, 54, 0.4)",
+                        "borderColor": "rgb(244, 67, 54)",
+                        "data": [
+                            1,
+                            2
+                        ]
+                    },
+                    {
+                        "label": "b",
+                        "backgroundColor": "rgba(33, 150, 243, 0.4)",
+                        "borderColor": "rgb(33, 150, 243)",
+                        "data": [
+                            2,
+                            4
+                        ]
+                    }
+                ]
+            }
+          })
+    })
+
+    it('array', () => {
+        const graph = autograph.line([
+            [1,2], 
+            [3,4]
+        ])
+        
+        assert.deepEqual(graph, {
+            "type": "line",
+            "data": {
+                "labels": [
+                    "0",
+                    "1"
+                ],
+                "datasets": [
+                    {
+                        "label": "0",
+                        "backgroundColor": "rgba(244, 67, 54, 0.4)",
+                        "borderColor": "rgb(244, 67, 54)",
+                        "data": [
+                            1,
+                            3
+                        ]
+                    },
+                    {
+                        "label": "1",
+                        "backgroundColor": "rgba(33, 150, 243, 0.4)",
+                        "borderColor": "rgb(33, 150, 243)",
+                        "data": [
+                            2,
+                            4
+                        ]
+                    }
+                ]
+            }
+        })
+    })
+})
